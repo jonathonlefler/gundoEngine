60ea2e30592cb93233c1d4f6bbddb6e8211cf1ee
Jonathon Lefler <32176683+jonathonlefler@users.noreply.github.com>
2019-05-13 16:48:23 -0400

    Delete Armor.java

--- src/Armor.java
+++
@@ -1,9 +0,0 @@
-//Jonathon Lefler
-
-public class Armor extends Equipables {
-	
-	public Armor(String name, int STR, int MAG, int DEF, int SPD, int MAN, int TP, int HP){
-		super(name, STR, MAG, DEF, SPD, MAN, TP, HP);
-
-	}
-}
