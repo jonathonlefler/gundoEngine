d9b9acb7b0bc82fcf8d4453a20c4ec13069a9ec5
Jonathon Lefler <32176683+jonathonlefler@users.noreply.github.com>
2019-05-13 16:50:23 -0400

    Delete Weapon.java

--- src/Weapon.java
+++
@@ -1,9 +0,0 @@
-//Jonathon Lefler
-
-public class Weapon extends Equipables {
-
-	public Weapon(String name, int STR, int MAG, int DEF, int SPD, int MAN, int TP, int HP){
-		super(name, STR, MAG, DEF, SPD, MAN, TP, HP);
-
-	}
-}
